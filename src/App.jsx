3fbbdb618fb28936992e030e3398b9a6a67727a8
LOCALHOST\91986 <shrihari3122003@gmail.com>
2024-05-18 12:06:35 +0530

    outlet added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
+
+const Layout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  )
+}
 function App() {
 
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/sign-in' element={<SignIn />}></Route>
-        <Route path='/sign-up' element={<SignUp />}></Route>
-        <Route path='/about' element={<About/>}></Route>
-        <Route path='/profile' element={<Profile/>}></Route>
+        <Route path='/' element={<Layout/>}>
+          <Route index element={<Home />}></Route>
+          <Route path='/sign-in' element={<SignIn />}></Route>
+          <Route path='/sign-up' element={<SignUp />}></Route>
+          <Route path='/about' element={<About />}></Route>
+          <Route path='/profile' element={<Profile />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
