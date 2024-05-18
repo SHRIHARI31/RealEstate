42e1f480a15b31e14ef941f0897d50a03d3248ed
LOCALHOST\91986 <shrihari3122003@gmail.com>
2024-05-18 11:49:34 +0530

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
 
 function App() {
 
 
   return (
-    <>
-      <h1  className='bg-black   text-white'>hello world</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/sign-in' element={<SignIn />}></Route>
+        <Route path='/sign-up' element={<SignUp />}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/profile' element={<Profile/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
